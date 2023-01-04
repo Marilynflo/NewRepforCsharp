a07fb6bd7fba64b316806f1b67c62298656b6275
mmf09 <mmf09@YPC>
2023-01-03 17:30:53 -0800

    Add project files.

---
+++ Method2/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method2
+{
+    class Class1
+    {
+        public class class1
+        {
+            public static int Sum(int number1, int number2)
+            {
+                int total;
+                total = number1 + number2;
+                return total;
+            }
+        }
+    }
+}
---
+++ Method2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Program
+    {
+         public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number if needed: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The sum of the numbers is : {0} ", number1 + number2);
+            Console.ReadLine();
+
+        }
+    }
+}
